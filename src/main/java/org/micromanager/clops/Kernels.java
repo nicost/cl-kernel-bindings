8a2ed3b19aad0484d557bc3054db8feae701b757
Nico <nico.stuurman@ucsf.edu>
2019-11-07 17:02:07 -0800

    Use kernels from org.micromanager rather than net.haesleinhuepf

--- src/main/java/org/micromanager/clops/Kernels.java
+++ src/main/java/org/micromanager/clops/Kernels.java
@@ -10,10 +10,10 @@ import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
 import net.haesleinhuepf.clij.clearcl.ClearCLHostImageBuffer;
 import net.haesleinhuepf.clij.clearcl.ClearCLImage;
 import net.haesleinhuepf.clij.clearcl.interfaces.ClearCLImageInterface;
-import net.haesleinhuepf.clij.OCLlib;
 import net.haesleinhuepf.clij.coremem.buffers.ContiguousBuffer;
 import net.haesleinhuepf.clij.coremem.enums.NativeTypeEnum;
 import net.haesleinhuepf.clij.coremem.offheap.OffHeapMemory;
+import org.micromanager.opencl.OCLlib;
 
 
 
